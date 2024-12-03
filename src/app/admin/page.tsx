312b33aacb84216c0ec49662c212cca0564e2703
Andry Ariadi <andryariadi23@gmail.com>
2024-12-03 12:35:18 +0700

    clean code tailwind css

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -16,8 +16,8 @@ const AdminPage = async () => {
   console.log(appointments, "<---diadminPage");
 
   return (
-    <div className="bg-violet-500 mx-auto max-w-7xl flex flex-col space-y-14">
-      <header className="bg-maber-500 admin-header">
+    <div className="b-violet-500 mx-auto max-w-7xl flex flex-col space-y-14">
+      <header className="b-amber-500 admin-header">
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="logo" width={35} height={35} />
@@ -27,13 +27,13 @@ const AdminPage = async () => {
         <p className="text-16-semibold">Admin Dashboard</p>
       </header>
 
-      <main className="bg-amber-700 admin-main">
-        <section className="bg-sky-600 w-full space-y-4">
+      <main className="b-amber-700 admin-main">
+        <section className="b-sky-600 w-full space-y-4">
           <h1 className="header">Welcome 👋</h1>
           <p className="text-dark-700">Start the day with managing new appointments</p>
         </section>
 
-        <section className="bg-emerald-500 admin-stat">
+        <section className="b-emerald-500 admin-stat">
           <StatCard icon={LuCalendarDays} color="text-yellow-500" bottom="-bottom-2" right="-right-3" colorBg="from-yellow-600 to-amber-900" title="Scheduled appointments" value={appointments?.scheduledCount} />
 
           <StatCard icon={GiSandsOfTime} color="text-cyan-500" bottom="-bottom-2" right="-right-6" colorBg="from-cyan-600 to-sky-900" title="Pending appointments" value={appointments?.pendingCount} />
